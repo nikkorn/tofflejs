a77160650f603332705eddfff62a74a1c3d8ef55
Nikolas Howard (student) <nh163@moco207L-b2.university.brighton.ac.uk>
2016-04-20 19:51:42 +0100

    Added first test to the grabValue() function spec.

--- toffle-jasmine-specs/ToffleJS-GrabValue-Spec.js
+++ toffle-jasmine-specs/ToffleJS-GrabValue-Spec.js
@@ -2,6 +2,8 @@ describe("Grab Value", function() {
     var preProcessedTemplatesObject;
 
     beforeEach(function() {
+        // Create a spy for the compileTemplate to stop it being called.
+        toffle.compileTemplate = jasmine.createSpy("compileTemplate spy");
         // grabValue() is a function of the object which is returned after successfully
         // pre-processing a template. Do a basic pre-processing of an empty template to get it.
         preProcessedTemplatesObject = toffle.template({
@@ -9,4 +11,8 @@ describe("Grab Value", function() {
         });
     });
     
+    it("should return 'undefined' when passed an undefined parameters object", function() {
+        var value = preProcessedTemplatesObject.grabValue(undefined, null);
+        expect(value).not.toBeDefined();
+    });
 }); 
