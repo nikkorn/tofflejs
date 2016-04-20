de0caa34102036b9fad8bb0ac0b2a2dd62f8566d
Nikolas Howard (student) <nh163@moco207L-b2.university.brighton.ac.uk>
2016-04-20 17:56:19 +0100

    Added missing configuration needed for karma coverage plugin.

--- my.conf.js
+++ my.conf.js
@@ -63,6 +63,12 @@ module.exports = function(config) {
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
     singleRun: false,
+    
+    plugins: [
+        'karma-jasmine',
+        'karma-firefox-launcher',
+        'karma-coverage'
+    ],
 
     // Concurrency level
     // how many browser should be started simultaneous
