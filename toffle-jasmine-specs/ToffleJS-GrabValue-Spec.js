53e6b5da0cec4ac3e0b0cbd1a076f39c5e7c6379
Nikolas Howard (student) <nh163@moco207L-b2.university.brighton.ac.uk>
2016-04-21 10:18:51 +0100

    Added unit test to spec for grabValue() which checks that is returns the expected value when passed a reference with a single identifier.

--- toffle-jasmine-specs/ToffleJS-GrabValue-Spec.js
+++ toffle-jasmine-specs/ToffleJS-GrabValue-Spec.js
@@ -15,4 +15,20 @@ describe("Grab Value", function() {
         var value = preProcessedTemplatesObject.grabValue(undefined, null);
         expect(value).not.toBeDefined();
     });
+    
+    it("should return the expected value when passed a reference with a single identifier", function() {
+        var testName = "test_name";
+        var testValue = "test_value";
+        // Construct our mock data object containing the variable we want the value of. 
+        var testValuesObject = {};
+        testValuesObject[testName] = testValue;
+        // Create the data structure that represents a property accessor.
+        var testRerefenceStructure = [{ 
+            name: testName, 
+            sub: []
+        }];
+        // Attempt to get the value.
+        var grabbedValue = preProcessedTemplatesObject.grabValue(testValuesObject, testRerefenceStructure);
+        expect(grabbedValue).toEqual(testValue);
+    });
 }); 
