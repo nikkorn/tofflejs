bdb7f907a985f83646c4fab5c7268b2cc57f54de
Nikolas Howard (student) <nh163@moco207L-b5.university.brighton.ac.uk>
2016-04-25 16:19:12 +0100

    Started writing integration tests, this commit will determine if Travis CI is happy with me generating my templates in the (hopefully) available DOM before running my tests.

--- toffle-jasmine-specs/ToffleJS-IntegrationTesting-Spec.js
+++ toffle-jasmine-specs/ToffleJS-IntegrationTesting-Spec.js
@@ -1,8 +1,60 @@
 
-describe("Integration Testing", function() {
-
-  describe("Go()", function() {
+describe("Template Pre-compilation", function() {
+    
+  beforeEach(function() {
+      
+  });
+    
+  it("works correctly when processing an individual empty template", function() {
+      var emptyTemplate = document.getElementById('EmptyTemplate');
+      if(emptyTemplate == null) {
+        populateDOMWithTemplateDefinitions();
+      }
+      //var preProcessedTemplate = toffle.template({
+          //template: document.getElementById('EmptyTemplate')
+      //});
+      expect(document.getElementById('EmptyTemplate')).not.toBe(null);
+  });
+  
+  it("works correctly when processing a template with a property accessor", function() {
       
   });
 
+  it("works correctly when processing an individual template with static content", function() {
+      
+  });
+  
+  it("works correctly when processing an individual template with an IF token", function() {
+      
+  });
+  
+  it("works correctly when processing an individual template with a NOT token", function() {
+      
+  });
+  
+  it("works correctly when processing an individual template with an EACH token", function() {
+      
+  });
+  
+  it("works correctly when processing an individual template with a HELPER token", function() {
+      
+  });
+  
+  it("works correctly when processing a template which references another template", function() {
+      
+  });
 }); 
+
+function populateDOMWithTemplateDefinitions() {
+  var templateContainer = document.createElement("div");
+  templateContainer.innerHTML = '<script id="EmptyTemplate" type="text/toffle-template"></script>' +
+    '<script id="EmptyTemplateWithArg" type="text/toffle-template" data-params="arg1"></script>' +
+    '<script id="TemplateWithPropertyAccessor" type="text/toffle-template"><^ testValue ^></script>' +
+    '<script id="TemplateWithIF" type="text/toffle-template"><^ if trueCondition ^>CONTENT<^/^></script>' +
+    '<script id="TemplateWithNOT" type="text/toffle-template"><^ not falseCondition ^>CONTENT<^/^></script>' +
+    '<script id="TemplateWithEACH" type="text/toffle-template"><^ each x in someJSONArray ^>CONTENT<^/^></script>' +
+    '<script id="TemplateWithMATCH" type="text/toffle-template"><^ match x in ??helper someJSONArray ^>CONTENT<^/^></script>' +
+    '<script id="TemplateWithPLUG" type="text/toffle-template"><^ plug EmptyTemplate ^></script>' +
+    '<script id="TemplateWithHELPER" type="text/toffle-template"><^ ??yIsTrue y ^>CONTENT<^/^></script>';
+    document.body.appendChild(templateContainer);
+};
