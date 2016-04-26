c18a46650a5f511c509c5cba124b5615f854a1c2
Nikolas Howard (student) <nh163@moco207L-b2.university.brighton.ac.uk>
2016-04-26 11:34:58 +0100

    When running the specs through Travis CI the DOM seems to be broken down when execution drops out of the suite it was defined in, fixed failing functional test by re-adding our empty template HTML element. This needs to be done for each relevant template element used as part of functional testing.

--- toffle-jasmine-specs/ToffleJS-TemplateExecution-Spec.js
+++ toffle-jasmine-specs/ToffleJS-TemplateExecution-Spec.js
@@ -1,6 +1,10 @@
 describe("Template Execution", function() {
     
   it("generates expected ouptut when evaluating an empty template", function() {
+      // We must append our template HTML element to the DOM.
+      var templateDiv = document.createElement("div");
+      templateDiv.innerHTML = '<script id="EmptyTemplate" type="text/toffle-template"></script>';
+      document.body.appendChild(templateDiv);
       // Get our empty template from the DOM
       var emptyTemplate = document.getElementById('EmptyTemplate');
       // Pre-process our empty template.
