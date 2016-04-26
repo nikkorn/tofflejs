19910aeb4420350345ec673fa9696ca96a7015cb
Nikolas Howard (student) <nh163@moco207L-b2.university.brighton.ac.uk>
2016-04-26 11:44:39 +0100

    Added functional test to check for correct output when evaluating a template that only contains static content.

--- toffle-jasmine-specs/ToffleJS-TemplateExecution-Spec.js
+++ toffle-jasmine-specs/ToffleJS-TemplateExecution-Spec.js
@@ -15,4 +15,22 @@ describe("Template Execution", function() {
       var result = preProcessedEmptyTemplate.go({});
       expect(result).toEqual("");
   });
+  
+  it("generates expected ouptut when evaluating a template wrapping static content", function() {
+      var testStaticContent = "STATIC_CONTENT";
+      // We must append our template HTML element to the DOM.
+      var templateDiv = document.createElement("div");
+      templateDiv.innerHTML = '<script id="FuncTestTemplateWithStaticContent" type="text/toffle-template">' +
+        testStaticContent + '</script>';
+      document.body.appendChild(templateDiv);
+      // Get our empty template from the DOM
+      var templateWithStaticContent = document.getElementById('FuncTestTemplateWithStaticContent');
+      // Pre-process our empty template.
+      var preProcessedEmptyTemplate = toffle.template({
+            template: templateWithStaticContent
+      });
+      // Evaluate our template, passing in empty dataset. We expect our static content as output.
+      var result = preProcessedEmptyTemplate.go({});
+      expect(result).toEqual(testStaticContent);
+  });
 });
