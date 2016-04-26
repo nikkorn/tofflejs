d57b1b67b679b935de15e75fcea0b80c4334872e
Nikolas Howard (student) <nh163@moco207L-b2.university.brighton.ac.uk>
2016-04-26 15:56:34 +0100

    Added functional test for evaluating a template that wraps an each statement.

--- toffle-jasmine-specs/ToffleJS-TemplateExecution-Spec.js
+++ toffle-jasmine-specs/ToffleJS-TemplateExecution-Spec.js
@@ -69,6 +69,7 @@ describe("Template Execution", function() {
             cond1: false,
             cond2: true
       };
+      // We must append our template HTML element to the DOM.
       var templateDiv = document.createElement("div");
       templateDiv.innerHTML = '<script id="FuncTestTemplateWithIfs" type="text/toffle-template">' +
       '<^ if cond1 ^>' + testFirstIfWrappedContent + '<^/^>' +
@@ -95,6 +96,7 @@ describe("Template Execution", function() {
             cond1: false,
             cond2: true
       };
+      // We must append our template HTML element to the DOM.
       var templateDiv = document.createElement("div");
       templateDiv.innerHTML = '<script id="FuncTestTemplateWithNots" type="text/toffle-template">' +
       '<^ not cond1 ^>' + testFirstNotWrappedContent + '<^/^>' + 
@@ -125,6 +127,7 @@ describe("Template Execution", function() {
             var3: 0,
             var4: 1
       };
+      // We must append our template HTML element to the DOM.
       var templateDiv = document.createElement("div");
       templateDiv.innerHTML = '<script id="FuncTestTemplateWithHelpers" type="text/toffle-template">' +
       '<^ ??isGreaterThanZero var1 ^>' + testFirstHelperWrappedContent + '<^/^>' + 
@@ -148,5 +151,42 @@ describe("Template Execution", function() {
       var result = preProcessedTemplate.go(testInputContext);
       // We are only expecting the second and fourth helper function to evaluate to true.
       expect(result).toEqual(testSecondHelperWrappedContent + testFourthHelperWrappedContent);
+  });
+  
+  it("generates expected ouptut when evaluating a template wrapping EACH statements", function() {
+      // Define an input context.
+      var testInputContext = {
+          testArray: [{
+                    ID: 0,
+                    val: "ZERO"
+                },
+                {
+                    ID: 1,
+                    val: "ONE"
+                },
+                {
+                    ID: 2,
+                    val: "TWO"
+                }]
+      };
+      // We must append our template HTML element to the DOM.
+      var templateDiv = document.createElement("div");
+      templateDiv.innerHTML = '<script id="FuncTestTemplateWithEACH" type="text/toffle-template">' +
+        '<^ each element in testArray ^><^ element.ID ^>:<^ element.val ^><^/^></script>';
+      document.body.appendChild(templateDiv);
+      // Get our empty template from the DOM
+      var initialTemplate = document.getElementById('FuncTestTemplateWithEACH');
+      // Pre-process our empty template and define the helper function we are using to test.
+      var preProcessedTemplate = toffle.template({
+            template: initialTemplate
+      });
+      // Evaluate our template, passing our test context. We expect the correct interpolated output. This
+      // would be the content wrapped by the each statement, processed for each element in the defined array.
+      var result = preProcessedTemplate.go(testInputContext);
+      // Concatenate our expected result.
+      var expectedResult = testInputContext.testArray[0].ID + ":" + testInputContext.testArray[0].val +
+      testInputContext.testArray[1].ID + ":" + testInputContext.testArray[1].val +
+      testInputContext.testArray[2].ID + ":" + testInputContext.testArray[2].val;
+      expect(result).toEqual(expectedResult);
   });
 });
